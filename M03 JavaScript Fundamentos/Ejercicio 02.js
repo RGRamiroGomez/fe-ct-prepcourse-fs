cc3a78fa5bb3a74e6aaeb7c29481ea9be0eb19b7
RamiroGomezJ <ramiro.gj.97@gmail.com>
2023-08-07 19:12:57 +0200

    Commit ejercicios modulo 03

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,8 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   var strin= "Hola"
+   return strin;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +13,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var summa= x + y;
+   return summa;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var rest=x-y;
+   return rest;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var div=x/y;
+   return div;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var multi=x*y;
+   return multi;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var mod=x%y;
+   return mod;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
